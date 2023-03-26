78dc54cb47ce61881c3b66c63392bbe34bfade80
Paul Niezen <pniezen@f0r5s15.codam.nl>
2023-03-26 08:57:14 +0200

    removed the try catch block :

--- Module_01/ex00/src/newZombie.cpp
+++ Module_01/ex00/src/newZombie.cpp
@@ -6,26 +6,17 @@
 /*   By: pniezen <pniezen@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/25 11:10:26 by pniezen       #+#    #+#                 */
-/*   Updated: 2023/02/25 12:38:43 by pniezen       ########   odam.nl         */
+/*   Updated: 2023/03/26 08:56:32 by pniezen       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Zombie.hpp>
 #include <iostream>
-#include <exception>
 
 Zombie	*newZombie(std::string name)
 {
 	Zombie	*zombie;
 
-	try
-	{
-		zombie = new Zombie(name);
-	}
-	catch (std::exception& e)
-	{
-		std::cerr << e.what() << std::endl;
-		exit(1);
-	}
+	zombie = new Zombie(name);
 	return (zombie);
 }
